2a38ae4ac9247a441db73da067d8f39f1a6bb669
Milan Humagain <10011027@BCS.NET.NZ>
2017-08-01 15:02:41 +1200

    First

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter firstname");
+            var Fname = Console.ReadLine();
+            Console.WriteLine(Fname);
+            Console.WriteLine("Enter lastname");
+            var Lname = Console.ReadLine();
+            Console.WriteLine(Lname);
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        
+        }
+    }
+}
